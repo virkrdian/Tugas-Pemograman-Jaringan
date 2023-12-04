5f1ad30b3913b8b593fcadc21e440f761385560c
Virki Fardian Nur Rohman <150001196+virkrdian@users.noreply.github.com>
2023-12-04 12:34:47 +0700

    Add files via upload

---
+++ Tugas Ke-1/src/Main.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    private static List<ClientHandler> clients = new ArrayList<>();
+
+    public static void main(String[] args) {
+        int portNumber = 12345;
+
+        try (ServerSocket serverSocket = new ServerSocket(portNumber)) {
+            System.out.println("Server is running on port " + portNumber);
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("New client connected");
+
+                ClientHandler clientHandler = new ClientHandler(clientSocket);
+                clients.add(clientHandler);
+                new Thread(clientHandler).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void broadcastMessage(String message, ClientHandler sender) {
+        for (ClientHandler client : clients) {
+            if (client != sender) {
+                client.sendMessage(message);
+            }
+        }
+    }
+}
